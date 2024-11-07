e7f7e311ae4e04b4755bda84f6d75b59a8a191a5
Ata Berkay Karakus <116452877+berkaykrks@users.noreply.github.com>
2024-11-07 11:53:12 +0300

    Menü ayarlandı

--- js/script.js
+++ js/script.js
@@ -19,6 +19,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const navbar = document.querySelector(".navbar");
 
     menuIcon.onclick = function() {
-        navbar.classList.toggle("active"); // Menü aç/kapa
+        navbar.classList.toggle("active"); // Toggle menu open/close
     };
+
+    // Hide the menu when scrolling
+    window.addEventListener("scroll", function() {
+        if (navbar.classList.contains("active")) {
+            navbar.classList.remove("active"); // Close the menu on scroll
+        }
+    });
 });
